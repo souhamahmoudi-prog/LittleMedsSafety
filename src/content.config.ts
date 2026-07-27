fece6ac27b38f5dfd47744d2d23041f1121da823
souhamahmoudi-prog <souha.mahmoudi@gmail.com>
2026-07-27 13:31:47 -0400

    update topics

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,5 +3,6 @@ import { defineCollection } from 'astro:content';
 const articles = defineCollection({});
 const resources = defineCollection({});
 const tools = defineCollection({});
+const topics = defineCollection({});
 
-export const collections = { articles, resources, tools };
+export const collections = { articles, resources, tools, topics };

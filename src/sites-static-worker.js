c142c1d169e46a9d69f6ebc4493da100345b7700
souhamahmoudi-prog <souha.mahmoudi@gmail.com>
2026-07-29 09:23:38 -0400

    Update sites-static-worker.js

--- src/sites-static-worker.js
+++ src/sites-static-worker.js
@@ -1,5 +1,14 @@
 export default {
   fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    if (path.endsWith('/')) {
+      url.pathname = `${path}index.html`;
+    } else if (!path.split('/').pop()?.includes('.')) {
+      url.pathname = `${path}/index.html`;
+    }
+
+    return env.ASSETS.fetch(new Request(url, request));
   },
 };
